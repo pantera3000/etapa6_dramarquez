db81ce5f464644d824eda7c4b0bf911dad84bfb7
pantera3000 <juancarloscn01@gmail.com>
2025-12-29 17:25:43 -0500

    fix: implementar whitenoise para servir archivos estáticos

--- consultorio_dental/settings_production.py
+++ consultorio_dental/settings_production.py
@@ -31,3 +31,8 @@ LOGGING = {
         },
     },
 }
+
+# CONFIGURACIÓN WHITENOISE (CRÍTICO PARA CPANEL)
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
